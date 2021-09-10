1fbc2ae9871dda94a2695ed19345f10e1dd7d435
valeyellow <sumit.mishtra.4693@gmail.com>
2021-09-10 17:46:37 +0530

    Added app.js file

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const poll = require("./routes/poll");
+
+// Set public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Enable CORS
+app.use(cors());
+
+app.use("/poll", poll);
+
+const port = 3000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server up and running on port ${port}`);
+});
